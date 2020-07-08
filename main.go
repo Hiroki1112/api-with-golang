1d6ae1d58d3ec416bc6525cf94d262bde4b55507
Hiroki1112 <pystdy@yahoo.co.jp>
2020-07-08 21:16:34 +0900

    7/8　作業分

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"reflect"
 	"strconv"
+	"fmt"
 
     "github.com/gorilla/mux"
 )
@@ -15,7 +16,7 @@ type Article struct {
 	ID int `json:id` 
 	Title string `json:title`
 	Author string `json:author`
-	PostDate string `json:year`
+	PostDate string `json:postdate`
 }
 
 var articles []Article//this is slice
@@ -44,11 +45,34 @@ func getArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func addArticle(w http.ResponseWriter, r *http.Request) {
-    log.Println("Add article is called")
+	//log.Println("Add article is called")
+	
+	var article Article
+
+	// json -> struct ?
+	json.NewDecoder(r.Body).Decode(&article)
+	fmt.Println("article:",article)
+
+	articles = append(articles, article)
+
+	// struct -> json ?
+    json.NewEncoder(w).Encode(articles)
+
 }
 
 func updateArticle(w http.ResponseWriter, r *http.Request) {
-    log.Println("Update article is called")
+	log.Println("Update article is called")
+	
+	var article Article
+	json.NewDecoder(r.Body).Decode(&article)
+
+	for i, item := range articles {
+		if item.ID == article.ID {
+			articles[i] = article
+		}
+	}
+
+	json.NewEncoder(w).Encode(article)
 }
 
 func removeArticle(w http.ResponseWriter, r *http.Request) {
@@ -60,13 +84,13 @@ func main() {
 	// ルーターを作成。リクエストを捌く。https://github.com/gorilla/mux
 	router := mux.NewRouter()
 
-	articles = append(articles,
+	/*articles = append(articles,
         Article{ID: 1, Title: "Article1", Author: "Gopher", PostDate: "2019/1/1"},
         Article{ID: 2, Title: "Article2", Author: "Gopher", PostDate: "2019/2/2"},
         Article{ID: 3, Title: "Article3", Author: "Gopher", PostDate: "2019/3/3"},
         Article{ID: 4, Title: "Article4", Author: "Gopher", PostDate: "2019/4/4"},
         Article{ID: 5, Title: "Article5", Author: "Gopher", PostDate: "2019/5/5"},
-    )
+    )*/
 
 	//エンドポイント
 	router.HandleFunc("/articles",getArticles).Methods("GET")
