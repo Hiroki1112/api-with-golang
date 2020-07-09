5bef2f909823511a72dde663e25146ccb36231c4
Hiroki1112 <pystdy@yahoo.co.jp>
2020-07-10 02:04:48 +0900

    API完全理解した

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # api-with-golang
+このサイトを参考にした。模写。
+https://qiita.com/__init__/items/145462cdbb0f569a29cf#-db-%E6%8E%A5%E7%B6%9A
--- main.go
+++ main.go
@@ -1,63 +1,89 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
     "log"
 	"net/http"
-	"reflect"
-	"strconv"
+	//"reflect"
+	//"strconv"
 	"fmt"
+	"os"
 
-    "github.com/gorilla/mux"//ルーティング用ライブラリ
+	"github.com/gorilla/mux"//ルーティング用ライブラリ
+	"github.com/lib/pq"
+    "github.com/subosito/gotenv"
 )
 
+var db *sql.DB
+
+func init() {
+	//作成した.envファイルを読み込む
+	gotenv.Load()
+}
+
+func logFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 type Article struct {
 	ID int `json:id` 
 	Title string `json:title`
 	Author string `json:author`
-	PostDate string `json:postdate`
+	PostDate string `json:year`
 }
 
 var articles []Article//this is slice
 
 func getArticles(w http.ResponseWriter, r *http.Request) {
 	//log.Printf("Get all articles")
-	json.NewEncoder(w).Encode(articles)
+	var article Article
+    articles = []Article{}
+
+    rows, err := db.Query("SELECT * FROM Article;")
+    logFatal(err)
+
+    defer rows.Close()
+
+    for rows.Next() {
+        err := rows.Scan(&article.ID, &article.Title, &article.Author, &article.PostDate)
+        logFatal(err)
+
+        articles = append(articles, article)
+    }
+    json.NewEncoder(w).Encode(articles)
 }
 
 func getArticle(w http.ResponseWriter, r *http.Request) {
 	log.Println("Get article is called")
 	
-	params := mux.Vars(r)
-	log.Println(params)
+	var article Article
+    params := mux.Vars(r)
 
-	log.Println(reflect.TypeOf(params["id"]))
+    rows := db.QueryRow("SELECT * FROM ARTICLE WHERE id=$1", params["id"])
 
-	i, _ := strconv.Atoi(params["id"])
+    err := rows.Scan(&article.ID, &article.Title, &article.Author, &article.PostDate)
+    logFatal(err)
 
-    // URL に指定した ID の情報を取得
-    for _, article := range articles {
-        if article.ID == i {
-            json.NewEncoder(w).Encode(&article)
-        }
-    }
+    json.NewEncoder(w).Encode(article)
 }
 
 func addArticle(w http.ResponseWriter, r *http.Request) {
 	//log.Println("Add article is called")
-	
 	var article Article
+	var articleID int
 
 	// json -> struct ?
 	json.NewDecoder(r.Body).Decode(&article)
-	fmt.Println("article:",article)
-
-	articles = append(articles, article)
-
-	// struct -> json ?
+	
+	err := db.QueryRow("INSERT INTO ARTICLE (title, author, postdate) values($1, $2, $3) RETURNING id;",
+		article.Title, article.Author, article.PostDate).Scan(&articleID)
+		
+	logFatal(err)
+	
     json.NewEncoder(w).Encode(articles)
-
 }
 
 func updateArticle(w http.ResponseWriter, r *http.Request) {
@@ -76,26 +102,32 @@ func updateArticle(w http.ResponseWriter, r *http.Request) {
 }
 
 func removeArticle(w http.ResponseWriter, r *http.Request) {
-	log.Println("Remove article is called")
-	
 	params := mux.Vars(r)
-	fmt.Println("params: ",params)
 
-	id,_ := strconv.Atoi(params['id'])
-	fmt.Println("id: ",id)
-	
-	fmt.Println("articles: ", articles)
+    result, err := db.Exec("DELETE FROM ARTICLE WHERE id=$1", params["id"])
+    logFatal(err)
 
-    for i, item := range articles {
-        if item.ID == id {
-            articles = append(articles[:i], articles[i+1:]...)
-        }
-    }
-    json.NewEncoder(w).Encode(articles)
+    rowsDeleted, err := result.RowsAffected()
+    logFatal(err)
+
+    fmt.Println("rowsDeleted", rowsDeleted)
+    json.NewEncoder(w).Encode(rowsDeleted)
 }
 
 
 func main() {
+
+	pgURL, err := pq.ParseURL(os.Getenv("ELEPHANTSQL_URL"))
+    logFatal(err)
+    log.Println("pgUrl: ", pgURL)
+
+    // Connect to postgres
+    db, err = sql.Open("postgres", pgURL)
+    logFatal(err)
+
+    err = db.Ping()
+    logFatal(err)
+
 	// ルーターを作成。リクエストを捌く。https://github.com/gorilla/mux
 	router := mux.NewRouter()
 
